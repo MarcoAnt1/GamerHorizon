1dbfc8163d7521142846e1e5d708d2fbefd73332
MarcoAnt1 <marco-antonio-souza@outlook.com>
2023-12-14 12:45:31 -0700

    Adjust page css and alignment.

--- gamerhorizon/src/app/app.component.ts
+++ gamerhorizon/src/app/app.component.ts
@@ -12,4 +12,23 @@ import { NavComponent } from '../shared/nav/nav.component';
 })
 export class AppComponent {
   title = 'Gamer Horizon';
+  games = [
+    {
+      name: "God Of War Ragnarok",
+      cover: "god-of-war-ragnarok-cover.jpg",
+      description: "This centers on Kratos, the God of War, and his now teenage son, Atreus, who are facing the conflict of the upcoming Ragnarök, due to the result of Kratos killing Norse God, Baldur, while setting on their to end tyranny reign of Odin, King of Asgard and while focusing off Atreus, who tries to uncover his origins"
+    },
+    {
+      name: "God Of War Ragnarok",
+      cover: "god-of-war-ragnarok-cover.jpg",
+      description: "This centers on Kratos, the God of War, and his now teenage son, Atreus, who are facing the conflict of the upcoming Ragnarök, due to the result of Kratos killing Norse God, Baldur, while setting on their to end tyranny reign of Odin, King of Asgard and while focusing off Atreus, who tries to uncover his origins"
+    },
+    {
+      name: "God Of War Ragnarok",
+      cover: "god-of-war-ragnarok-cover.jpg",
+      description: "This centers on Kratos, the God of War, and his now teenage son, Atreus, who are facing the conflict of the upcoming Ragnarök, due to the result of Kratos killing Norse God, Baldur, while setting on their to end tyranny reign of Odin, King of Asgard and while focusing off Atreus, who tries to uncover his origins"
+    },
+
+  ];
+
 }
